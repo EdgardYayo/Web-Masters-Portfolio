adbcd45a6c34591a1eeb0e9417c3dfb2686e5411
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-02-15 21:34:13 -0800

    scrolled and focus on the nav titles fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Landing from "./components/Landing/Landing"
-import AboutUs from "./components/AboutUs/AboutUs"
-import Services from "./components/Services/Services"
-import Skills   from "./components/Skills/Skills"
-import Portfolio from "./components/Portfolio/Portfolio"
-import Contact from "./components/Contact/Contact"
-import Footer  from "./components/Footer/Footer"
+import Landing from "./components/Landing/Landing";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Services from "./components/Services/Services";
+import Skills from "./components/Skills/Skills";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
