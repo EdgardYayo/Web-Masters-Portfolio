30888968baae2ed39484c441ead6f390198df6fc
Daniela Pourrieux <dhannychocolateteamo@gmail.com>
2023-02-27 20:41:07 -0300

    modificacion de skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,55 @@
 import React from "react";
 import style from "./Skills.module.css";
+import { SiTailwindcss, SiRedux, SiTypescript, SiExpress, SiPostgresql, SiJavascript, SiAdobephotoshop, SiAdobeaftereffects } from 'react-icons/si';
+import { DiMongodb, DiHtml5, DiCss3,DiSass, DiTrello } from "react-icons/di"
+import { FaNodeJs, FaReact, FaGitAlt } from "react-icons/fa"
+import { FiFigma } from "react-icons/fi"
 
 const Skills = () => {
 
 	return (
 		<div className={style["contenedor-skills"]} id={style.skills}>
 			<h3>SKILLS</h3>
-			<div className={style.skill}>
+			<div className={style.ico_container}>
+			<FaReact id={style.ico}/>
+			<SiJavascript id={style.ico}/>
+			<DiHtml5 id={style.ico}/>
+			<DiCss3 id={style.ico}/>
+			<DiSass id={style.ico}/>
+			<SiTailwindcss id={style.ico}/>
+			<SiRedux id={style.ico}/>
+			<DiMongodb id={style.ico}/>
+			<SiTypescript id={style.ico}/>
+			<SiExpress id={style.ico}/>
+			<FaNodeJs id={style.ico}/>
+			<SiPostgresql id={style.ico}/>	
+			<FiFigma id={style.ico}/>	
+			<SiAdobephotoshop id={style.ico}/>
+			<SiAdobeaftereffects id={style.ico}/>
+			<FaGitAlt id={style.ico}/>
+			<DiTrello id={style.ico}/>	
+			</div>
+		</div>
+	);
+};
+
+export default Skills;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className={style.skill}>
 				<div className={style.info}>
 					<p>
 						<span className={style.lista}> </span>Html & Css
@@ -53,9 +96,4 @@ const Skills = () => {
 				<div className={style.barra}>
 					<div className={style["barra-progreso4"]} id={style.ps}></div>
 				</div>
-			</div>
-		</div>
-	);
-};
-
-export default Skills;
+			</div> */}
